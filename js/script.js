36ec3ce3af983b4319341b7fac19b5fa19359bbd
Vixowow <vixowow.nmr@gmail.com>
2026-06-08 02:37:35 -0400

    feat: add localStorage persistance

--- js/script.js
+++ js/script.js
@@ -1,52 +1,63 @@
 const form = document.querySelector("#sessionForm");
 const sessionsContainer = document.querySelector("#sessionsContainer");
-const sesiones = [];
+
+let sesiones = [];
+
+const sesionesGuardadas = localStorage.getItem("sesionesNMR");
+
+if(sesionesGuardadas) {
+    sesiones =  JSON.parse(sesionesGuardadas);
+}
+
+
+console.log(sesionesGuardadas);
 
 function renderizarSesiones() {
     sessionsContainer.innerHTML  = "";
 
     sesiones.forEach(function(sesion) {
-            const card = document.createElement("div");
-            card.classList.add("session-card");
+        const card = document.createElement("div");
+        card.classList.add("session-card");
 
-            const titulo = document.createElement("h3");
-            const artistaInfo = document.createElement("p");
-            const estudioInfo = document.createElement("p");
-            const fechaInfo = document.createElement("p");
-            const horaInfo = document.createElement("p");
-            const estadoInfo = document.createElement("p");
-            const productoresInfo = document.createElement("p");
-            const serviciosInfo = document.createElement("p");
-            const saldoInfo = document.createElement("p");
+        const titulo = document.createElement("h3");
+        const artistaInfo = document.createElement("p");
+        const estudioInfo = document.createElement("p");
+        const fechaInfo = document.createElement("p");
+        const horaInfo = document.createElement("p");
+        const estadoInfo = document.createElement("p");
+        const productoresInfo = document.createElement("p");
+        const serviciosInfo = document.createElement("p");
+        const saldoInfo = document.createElement("p");
 
+        titulo.textContent = sesion.proyecto;
+        artistaInfo.textContent = sesion.artista;
+        estudioInfo.textContent = sesion.estudio;
+        fechaInfo.textContent = `Fecha: ${sesion.fecha}`;
+        horaInfo.textContent = `Horario: ${sesion.horaInicio} - ${sesion.horaTermino}`;
+        productoresInfo.textContent = `Productores: ${sesion.productores.join(", ")}`;
+        serviciosInfo.textContent = `Servicios: ${sesion.servicios.join(", ")}`;
+        estadoInfo.textContent = `Pago: ${sesion.estadoPago}`;
+        saldoInfo.textContent = `Saldo pendiente: ${sesion.saldoPendiente}`;
 
-            titulo.textContent = sesion.proyecto;
-            artistaInfo.textContent = sesion.artista;
-            estudioInfo.textContent = sesion.estudio;
-            fechaInfo.textContent = `Fecha: ${sesion.fecha}`;
-            horaInfo.textContent = `Horario: ${sesion.horaInicio} - ${sesion.horaTermino}`;
-            productoresInfo.textContent = `Productores: ${sesion.productores.join(", ")}`;
-            serviciosInfo.textContent = `Servicios: ${sesion.servicios.join(", ")}`;
-            estadoInfo.textContent = `Pago: ${sesion.estadoPago}`;
-            saldoInfo.textContent = `Saldo pendiente: ${sesion.saldoPendiente}`;
+        console.log(card);
 
-            console.log(card);
+        card.appendChild(titulo);
+        card.appendChild(artistaInfo);
+        card.appendChild(estudioInfo);
+        card.appendChild(fechaInfo);
+        card.appendChild(horaInfo);
+        card.appendChild(productoresInfo);
+        card.appendChild(serviciosInfo);
+        card.appendChild(estadoInfo);
+        card.appendChild(saldoInfo);
 
-            card.appendChild(titulo);
-            card.appendChild(artistaInfo);
-            card.appendChild(estudioInfo);
-            card.appendChild(fechaInfo);
-            card.appendChild(horaInfo);
-            card.appendChild(productoresInfo);
-            card.appendChild(serviciosInfo);
-            card.appendChild(estadoInfo);
-            card.appendChild(saldoInfo);
-
-            sessionsContainer.appendChild(card);
+        sessionsContainer.appendChild(card);
         
     });
 
 };
+
+renderizarSesiones();
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -99,18 +110,10 @@ form.addEventListener("submit", function(event) {
 
     renderizarSesiones();
 
-    console.log(sesiones.length);
+    localStorage.setItem("sesionesNMR", JSON.stringify(sesiones));
 
     console.log(sesiones);
 
-    console.log(sesion);
-
-    console.log(proyecto);
-
-
-
-
-
-
 });
+
 
